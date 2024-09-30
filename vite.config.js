f6bb8ec2c23fe8d5ddb79cdc06c62b27fd2541f8
Chambers <bobhus394@gmail.com>
2024-09-30 10:21:00 +0800

    delete base url config

--- vite.config.js
+++ vite.config.js
@@ -34,8 +34,5 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  base: process.env.NODE_ENV === 'production'
-    ? '/remote-job-dashboard/'
-    : '/'
+  }
 })
